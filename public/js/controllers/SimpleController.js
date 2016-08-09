3b3e1ad76ded16db0eb939ba480b98dcb1f886f4
Justin Dennison <justin1dennison@gmail.com>
2016-08-09 17:57:18 -0400

    started user service

--- public/js/controllers/SimpleController.js
+++ public/js/controllers/SimpleController.js
@@ -3,9 +3,25 @@
   angular.module('ngBlog')
         .controller('SimpleController', SimpleController);
 
-  SimpleController.$inject = ['$scope'];
+  SimpleController.$inject = ['$scope', 'UserService'];
 
-  function SimpleController($scope){
+  function SimpleController($scope, UserService){
     $scope.message = 'Hey! Angular Works!';
+
+    UserService.getAllUsers()
+              .then(function(response){
+                console.log(response);
+              });
+    var userObj = {
+      firstName: 'Lil',
+      lastName: 'Jon',
+      age: 41,
+      email: 'lil@jon434365.com'
+    };
+    UserService.createUser(userObj)
+              .then(function(response){
+                console.log(response);
+              });
+
   }
 })();
